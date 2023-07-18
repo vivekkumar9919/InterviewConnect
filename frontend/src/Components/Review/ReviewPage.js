498a856d021ca74f9e2956d52f1db42f7b8f9c49
vivekkumar9919 <prajapativk9919@gmail.com>
2023-07-18 12:33:52 +0530

    Review is added

--- frontend/src/Components/Review/ReviewPage.js
+++ frontend/src/Components/Review/ReviewPage.js
@@ -12,14 +12,14 @@ export default function ReviewPage() {
                             <div className="carousel-inner">
                                 <div className="carousel-item active">
                                     <div className='textBox'>
-                                        <div id='reviewText'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, asperiores. Commodi iusto, dolor eveniet vel neque ipsa explicabo ea eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, similique.</div>
-                                        <div id='personName'><i>-Vivek Kumar </i></div>
+                                        <div id='reviewText'> I think this is a really great site for learning. In compared to other sites found on the internet, it is very easy to use without any hassle, annoying pop-ups, or paid services. Great work!</div>
+                                        <div id='personName'><i>-Prasoon Dubey</i></div>
                                     </div>
                                 </div>
                                 <div className="carousel-item">
                                     <div className='textBox'>
-                                        <div id='reviewText'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, asperiores. Commodi iusto, dolor eveniet vel neque ipsa explicabo ea eaque!</div>
-                                        <div id='personName'><i>-Vivek Kumar </i></div>
+                                        <div id='reviewText'> A one stop platform to learn and prepare all types of questions for interview.Thanks to Interviewconnect teams for providing a such a amazing platform</div>
+                                        <div id='personName'><i>-Akhil Kumar Maurya </i></div>
                                     </div>
                                 </div>
                                 <div className="carousel-item">
